bdba7cfe19cd0a934ffed5a4260c11b4b35781b0
zndrmn <zanderman007.zm@gmail.com>
2022-02-27 21:47:04 -0600

    Added Item

--- src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
+++ src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
@@ -1,6 +1,7 @@
 package net.zndrmn.diamondingots;
 
 import net.fabricmc.api.ModInitializer;
+import net.zndrmn.diamondingots.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,8 @@ public class DiamondIngots implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "diamondingots";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +18,7 @@ public class DiamondIngots implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Items.registerItems();
+
 	}
 }
---
+++ src/main/java/net/zndrmn/diamondingots/item/Items.java
@@ -0,0 +1,28 @@
+package net.zndrmn.diamondingots.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.zndrmn.diamondingots.DiamondIngots;
+import org.lwjgl.system.CallbackI;
+
+public class Items {
+
+    public static final Item DIAMOND_INGOT = registerItem("diamond_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+
+        return Registry.register(Registry.ITEM, new Identifier(DiamondIngots.MOD_ID, name), item);
+
+    }
+
+    public static void registerItems() {
+
+        DiamondIngots.LOGGER.info("Registering items for " + DiamondIngots.MOD_ID);
+
+    }
+
+}
