330da863c104f212525a649bbeb9479625658003
zndrmn <zanderman007.zm@gmail.com>
2022-11-16 21:11:10 -0600

    Initial port for forge-1.19.2

--- src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
+++ src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
@@ -1,22 +1,38 @@
 package net.zndrmn.diamondingots;
 
-import net.fabricmc.api.ModInitializer;
-import net.zndrmn.diamondingots.registry.Items;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class DiamondIngots implements ModInitializer {
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DiamondIngots.MOD_ID)
+public class DiamondIngots {
+    public static final String MOD_ID = "diamondingots";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-	public static final String MOD_ID = "diamondingots";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public DiamondIngots() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        modEventBus.addListener(this::commonSetup);
 
-		Items.registerItems();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
-	}
+    }
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
 }
