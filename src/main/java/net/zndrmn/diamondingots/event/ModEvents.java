57280716fe9d19499f6c180cfbef2dee17856801
bruhmom1 <loganhzart@gmail.com>
2022-11-23 14:04:05 -0600

    bumped version / 1.4-forge.66+1.19.2

--- src/main/java/net/zndrmn/diamondingots/event/ModEvents.java
+++ src/main/java/net/zndrmn/diamondingots/event/ModEvents.java
@@ -1,14 +1,81 @@
 package net.zndrmn.diamondingots.event;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.zndrmn.diamondingots.DiamondIngots;
 
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = DiamondIngots.MOD_ID)
 public class ModEvents {
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event) {
+        if (event.getType() == VillagerProfession.ARMORER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 3;
 
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_INGOT.get(), 1),
+                    stack, 12, 30, 0.05F));
+
+        }
+
+        if (event.getType() == VillagerProfession.ARMORER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 3;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_SHARD.get(), 2),
+                    stack, 12, 30, 0.05F));
+
+        }
+        if (event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_INGOT.get(), 1),
+                    stack, 12, 30, 0.05F));
+
+        }
+        if (event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_SHARD.get(), 2),
+                    stack, 12, 30, 0.05F));
+        }
+        if (event.getType() == VillagerProfession.WEAPONSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_INGOT.get(), 1),
+                    stack, 12, 30, 0.05F));
+        }
+        if (event.getType() == VillagerProfession.WEAPONSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(Items.EMERALD, 1);
+            int villagerLevel = 4;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(net.zndrmn.diamondingots.registry.Items.DIAMOND_SHARD.get(), 2),
+                    stack, 12, 30, 0.05F));
+        }
     }
 }
+
