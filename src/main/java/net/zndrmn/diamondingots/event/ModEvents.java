7728b15aa8a7b4148955f54fdf09fb0581922ad9
zndrmn <zanderman007.zm@gmail.com>
2022-11-23 15:09:08 -0600

    Optimized Imports

--- src/main/java/net/zndrmn/diamondingots/event/ModEvents.java
+++ src/main/java/net/zndrmn/diamondingots/event/ModEvents.java
@@ -3,7 +3,6 @@ package net.zndrmn.diamondingots.event;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
