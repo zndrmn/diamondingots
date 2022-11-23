3e7421a40549ef338993ee86fb3e9004d3924e3e
zndrmn <zanderman007.zm@gmail.com>
2022-11-23 14:55:24 -0600

    Basic Implementation of owo-config Added "owo-lib" Added "config/ConfigModel.java"

--- src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
+++ src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
@@ -1,6 +1,7 @@
 package net.zndrmn.diamondingots;
 
 import net.fabricmc.api.ModInitializer;
+import net.zndrmn.diamondingots.config.ConfigBuilder;
 import net.zndrmn.diamondingots.registry.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +11,10 @@ public class DiamondIngots implements ModInitializer {
 	public static final String MOD_ID = "diamondingots";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final ConfigBuilder CONFIG = ConfigBuilder.createAndLoad();
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		Items.registerItems();
 
---
+++ src/main/java/net/zndrmn/diamondingots/config/ConfigModel.java
@@ -0,0 +1,10 @@
+package net.zndrmn.diamondingots.config;
+
+import io.wispforest.owo.config.annotation.Config;
+
+@Config(name = "diamondingots", wrapperName = "ConfigBuilder")
+public class ConfigModel {
+
+    public boolean aBooleanToggle = false;
+
+}
