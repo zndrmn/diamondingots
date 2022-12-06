6a8aefe0552af5b9f2c13b85eac961e4310da457
zndrmn <zanderman007.zm@gmail.com>
2022-12-05 20:26:57 -0600

    Initial fabric/1.19.3 commit updated: "build.gradle" updated: "DiamondIngots.java" updated: "gradle.properties" updated: "Items.java" missing: "ConfigLootTables.java" missing: "ConfigModel.java" missing: "OptionsLootTables.java"

--- src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
+++ src/main/java/net/zndrmn/diamondingots/DiamondIngots.java
@@ -1,8 +1,6 @@
 package net.zndrmn.diamondingots;
 
 import net.fabricmc.api.ModInitializer;
-import net.zndrmn.diamondingots.config.ConfigBuilder;
-import net.zndrmn.diamondingots.config.ConfigLootTables;
 import net.zndrmn.diamondingots.registry.Events;
 import net.zndrmn.diamondingots.registry.Items;
 import org.slf4j.Logger;
@@ -13,18 +11,18 @@ public class DiamondIngots implements ModInitializer {
 	public static final String MOD_ID = "diamondingots";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final int CONFIG_VERSION = 1;
-	public static final ConfigBuilder CONFIG = ConfigBuilder.createAndLoad();
+	//public static final int CONFIG_VERSION = 1;
+	//public static final ConfigBuilder CONFIG = ConfigBuilder.createAndLoad();
 
 	@Override
 	public void onInitialize() {
 
 		Items.registerItems();
 		Events.registerEvents();
-		ConfigLootTables.modifyLootTables();
+		//ConfigLootTables.modifyLootTables();
 
-		if (CONFIG.config_version() != CONFIG_VERSION) {
-			LOGGER.error("The config version is outdated! Please delete or change the config version.");
-		}
+		//if (CONFIG.config_version() != CONFIG_VERSION) {
+		//	LOGGER.error("The config version is outdated! Please delete or change the config version.");
+		//}
 	}
 }
