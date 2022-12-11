f8806cc06f4c369e437ba4ee46969389532f014a
zndrmn <zanderman007.zm@gmail.com>
2022-12-10 19:35:42 -0600

    Updated: "Items.java" Added "diamond_ingots" & "diamond_shards" to vanilla INGREDIENTS ItemGroup

--- src/main/java/net/zndrmn/diamondingots/registry/Items.java
+++ src/main/java/net/zndrmn/diamondingots/registry/Items.java
@@ -1,6 +1,8 @@
 package net.zndrmn.diamondingots.registry;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -12,10 +14,11 @@ public class Items {
     public static final Item DIAMOND_SHARD = registerItem("diamond_shard", new Item((new Item.Settings())));
 
     private static Item registerItem(String name, Item item) {
-
         return Registry.register(Registries.ITEM, new Identifier(DiamondIngots.MOD_ID, name), item);
-
     }
+
     public static void registerItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(DIAMOND_INGOT));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(DIAMOND_SHARD));
     }
 }
